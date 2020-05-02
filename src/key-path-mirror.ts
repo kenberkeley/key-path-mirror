b78abf1f680d6b74c22cf110ef62705d939da09a
Ken Huang <kenhuang.au@gmail.com>
2020-05-02 16:19:07 +1000

    fix(type): return type

--- src/key-path-mirror.ts
+++ src/key-path-mirror.ts
@@ -1,7 +1,14 @@
 type Obj = { [key: string]: any }
 
+type RecursiveSetObjValTypeAsStr<O extends Obj> = {
+  [K in keyof O]: O[K] extends Obj ? RecursiveSetObjValTypeAsStr<O[K]> : string
+}
+
 export default keyPathMirror
-export function keyPathMirror<T extends Obj> (obj: T, prefixPath?: string): T {
+export function keyPathMirror<
+  T extends Obj,
+  R extends RecursiveSetObjValTypeAsStr<T>
+> (obj: T, prefixPath?: string): R {
   if (!isObject(obj)) {
     throw new Error('Argument must be an object')
   }
@@ -15,7 +22,7 @@ export function keyPathMirror<T extends Obj> (obj: T, prefixPath?: string): T {
       ? keyPathMirror(val as Obj, curPath)
       : curPath
   })
-  return keyPathMirroredObj as T
+  return keyPathMirroredObj as R
 }
 
 function isObject (o: any) {
