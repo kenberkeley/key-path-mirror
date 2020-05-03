f3866da2ccdfda8050ae844dc1f125fe78b7d040
Ken Huang <kenhuang.au@gmail.com>
2020-05-03 14:57:25 +1000

    build: introduce husky + lint-staged

--- src/key-path-mirror.ts
+++ src/key-path-mirror.ts
@@ -5,16 +5,19 @@ type RecurSetObjValTypeAsStr<O extends Obj> = {
   [K in keyof O]: O[K] extends Obj ? RecurSetObjValTypeAsStr<O[K]> : string
 }
 type RetType<O extends Obj, P extends Prefix> = {
-  [K in keyof O]: O[K] extends Obj ? RecurSetObjValTypeAsStr<O[K]> : (
-    P extends undefined
-      ? K // imitate @types/keymirror: https://git.io/JfsJx
-      : string
-  )
+  [K in keyof O]: O[K] extends Obj
+    ? RecurSetObjValTypeAsStr<O[K]>
+    : P extends undefined
+    ? K // imitate @types/keymirror: https://git.io/JfsJx
+    : string
 }
-export function keyPathMirror<T extends Obj>(obj: T): RetType<T, undefined>
-export function keyPathMirror<T extends Obj, P extends string>(obj: T, prefix: P): RetType<T, P>
+export function keyPathMirror<T extends Obj> (obj: T): RetType<T, undefined>
+export function keyPathMirror<T extends Obj, P extends string> (
+  obj: T,
+  prefix: P
+): RetType<T, P>
 
-export function keyPathMirror(obj: Obj, prefix?: string) {
+export function keyPathMirror (obj: Obj, prefix?: string) {
   if (!isObject(obj)) {
     throw new Error('1st argument should be an object')
   }
